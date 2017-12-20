cb9e6594d3f0da9398f63760069a883ca0a0bb97
UltraJohn <johnh.dk93@gmail.com>
2017-12-20 14:47:25 +0100

    Add project files.

---
+++ AuctionWebApplication/Controllers/AuctionController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AuctionWebApplication.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace AuctionWebApplication.Controllers
+{
+    public class AuctionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpGet]
+		public IActionResult Bid(int id)
+		{
+			if (id <= 0) return RedirectToAction("index");
+			AuctionService.AuctionsServiceClient client = new AuctionService.AuctionsServiceClient();
+			AuctionService.AuctionItem item = client.GetAuctionItemAsync(id).Result;
+			if (item == null) return RedirectToAction("index");
+			ViewBag.id = item.ItemNumber;
+			ViewBag.itemdesc = item.ItemDescription;
+			ViewBag.ratingprice = item.RatingPrice;
+			return View();
+		}
+		[HttpPost]
+		public IActionResult SendBid(string BidCustomName, string BidCustomPhone, int ItemNumber, int BidPrice)
+		{
+			List<BidProvider> bids;
+			if (HttpContext.Session.GetString("bids") == null)
+			{
+				bids = new List<BidProvider>();
+			}
+			else
+			{
+				bids = JsonConvert.DeserializeObject<List<BidProvider>>(HttpContext.Session.GetString("bids"));
+			}
+			BidProvider bid = new BidProvider() {BidCustomName = BidCustomName, BidCustomPhone = BidCustomPhone, ItemNumber = ItemNumber, BidPrice = BidPrice, BidTimestamp = DateTime.Now };
+			bids.Add(bid);
+			HttpContext.Session.SetString("bids", JsonConvert.SerializeObject(bids));
+			AuctionService.AuctionsServiceClient client = new AuctionService.AuctionsServiceClient();
+			client.ProvideBidAsync(ItemNumber, BidPrice, BidCustomName, BidCustomPhone);
+			return RedirectToAction("index");
+		}
+
+		public IActionResult MyBids()
+		{
+			string json = HttpContext.Session.GetString("bids");
+			if (json == null) return RedirectToAction("index");
+
+			List<BidProvider> bids = JsonConvert.DeserializeObject<List<BidProvider>>(json);
+			ViewBag.bids = bids;
+			return View();
+		}
+    }
+}
---
+++ AuctionWebApplication/Models/BidProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionWebApplication.Models
+{
+    public class BidProvider
+    {
+		[Required]
+		[Display(Name="Bid Custom Name")]
+		[MinLength(4)]
+		public string BidCustomName { get; set; }
+
+		[Required]
+		[Display(Name = "Bid Custom Phone")]
+		[MinLength(8)]
+		[MaxLength(8)]
+		public string BidCustomPhone { get; set; }
+
+		[Required]
+		[Display(Name = "Item Number")]
+		public int ItemNumber { get; set; }
+
+		[Required]
+		[Display(Name = "Bid Price")]
+		public int BidPrice{ get; set; }
+
+		public DateTime BidTimestamp { get; set; }
+	}
+}
